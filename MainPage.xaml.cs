c255efb40fa8d73fb839c15f4d66af0a15ba8567
Gabriele <gabboxl0@gmail.com>
2023-02-27 11:45:57 +0100

    Added new api login

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using ClassevivaPCTO.Utils;
+using Refit;
+using System;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
 
@@ -25,6 +18,32 @@ namespace ClassevivaPCTO
         public MainPage()
         {
             this.InitializeComponent();
+
+
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IClassevivaAPI api = RestService.For<IClassevivaAPI>("https://web.spaggiari.eu/rest/v1");
+
+            var measurement = new LoginData
+            {
+                Uid = "",
+                Pass = "",
+            };
+
+            LoginResult user = api.LoginAsync(measurement).Result;
+
+
+
+
+            MessageDialog dialog = new MessageDialog("a " + user.FirstName + user.LastName);
+            dialog.Commands.Add(new UICommand("Yes", null));
+            dialog.Commands.Add(new UICommand("No", null));
+            dialog.DefaultCommandIndex = 0;
+            dialog.CancelCommandIndex = 1;
+            var cmd = await dialog.ShowAsync();
+
         }
     }
 }
