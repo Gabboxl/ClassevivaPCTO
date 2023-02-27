04965c4584da4e0c14c03c5878f80abb4065f361
Gabriele <gabboxl0@gmail.com>
2023-02-27 11:45:57 +0100

    Added new api login

---
+++ Utils/Class1.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace ClassevivaPCTO.Utils
+{
+    public class LoginData
+    {
+
+        [AliasAs("uid")]
+        public string Uid { get; set; }
+
+        [AliasAs("pass")]
+        public string Pass { get; set; }
+
+    }
+
+
+    public class LoginResult
+    {
+        public string Ident { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Token { get; set; }
+
+        [JsonProperty("release")]
+        public DateTime Release { get; set; }
+
+        [JsonProperty("expire")]
+        public DateTime Expire { get; set; }
+    }
+
+
+    [Headers("User-Agent: zorro/1.0", "Z-Dev-Apikey: +zorro+", "Content-Type: application/json")]
+    public interface IClassevivaAPI
+    {
+
+        [Post("/auth/login/")]
+        Task<LoginResult> LoginAsync([Body(BodySerializationMethod.Serialized)] LoginData logdata);
+    }
+
+}
