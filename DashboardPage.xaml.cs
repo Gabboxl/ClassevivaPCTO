5fb8482dcd98602ba63e7c1eb28ad6ff046bab40
Gabriele <gabboxl0@gmail.com>
2023-02-27 13:10:28 +0100

    Added new page parameters

--- DashboardPage.xaml.cs
+++ DashboardPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassevivaPCTO.Utils;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,6 +26,31 @@ namespace ClassevivaPCTO
         public DashboardPage()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            LoginResult parameters = (LoginResult)e.Parameter;
+
+            textBenvenuto.Text = "Benvenuto " + UppercaseFirst(parameters.FirstName) + " " + UppercaseFirst(parameters.LastName);
+            
+
+        }
+
+
+        static string UppercaseFirst(string s)
+        {
+            // Check for empty string.
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+
+            s = s.ToLower();
+            // Return char and concat substring.
+            return char.ToUpper(s[0]) + s.Substring(1);
         }
     }
 }
