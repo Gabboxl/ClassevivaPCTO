4c0263145e1f7b9623a1b708223150d15e29be0f
Gabriele <gabboxl0@gmail.com>
2023-03-01 13:48:37 +0100

    Added new grades things

---
+++ Converters/GradeToColorConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace ClassevivaPCTO.Converters
+{ 
+        public class GradeToColorConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, string language)
+            {
+                SolidColorBrush brush = new SolidColorBrush();
+                float grade = (float)value;
+
+                if (grade >= 6)
+                {
+                    brush.Color = Colors.Teal;
+                }
+                else if(grade >= 5)
+            {
+                brush.Color = Colors.Orange;
+            } else
+                {
+                    brush.Color = Colors.Red;
+                }
+                return brush;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, string language)
+            {
+                return new NotImplementedException();
+            }
+        }
+}
