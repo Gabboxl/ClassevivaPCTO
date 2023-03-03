11136a55d09a9ccfe3833f32a2df5880c065380f
Gabriele <gabboxl0@gmail.com>
2023-03-03 10:34:36 +0100

    Added ViewModel and ViewModelHolder classes
    + cleaned up code styles

---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ClassevivaPCTO.Utils;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+
+namespace ClassevivaPCTO.ViewModel
+{
+    public class ViewModel : ObservableObject
+    {
+        private LoginResult loginResult;
+
+
+        public LoginResult LoginResult
+        {
+            get => loginResult;
+            set => SetProperty(ref loginResult, value);
+        }
+
+    }
+}
---
+++ ViewModel/ViewModelHolder.cs
@@ -0,0 +1,28 @@
+﻿using ClassevivaPCTO.Utils;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+
+namespace ClassevivaPCTO.ViewModel
+{
+    public static class ViewModelHolder
+    {
+        private static ViewModel viewModel;
+
+        public static ViewModel getViewModel()
+        {
+            if(viewModel == null)
+            {
+                viewModel = new ViewModel();
+            }
+
+            return viewModel;
+        }
+
+
+    }
+}
