742a4b220a3b0549c3cc544db491e579d3819f06
Gabriele <gabboxl0@gmail.com>
2023-03-15 08:14:16 +0100

    fix calcolo media

--- Utils/VariousUtils.cs
+++ Utils/VariousUtils.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,5 +21,61 @@ namespace ClassevivaPCTO.Utils
             // Return char and concat substring.
             return char.ToUpper(s[0]) + s.Substring(1);
         }
+
+        public static float? GradeToInt(object value)
+        {
+            float? valore = null;
+
+            if (value is Grade)
+            {
+                Grade grade = (Grade)value;
+
+                if (grade.decimalValue != null)
+                {
+                    valore = grade.decimalValue;
+                }
+                else if (!grade.displayValue.ToLower().Equals("nv"))
+                {
+
+                    switch (grade.displayValue)
+                    {
+                        case "o":
+                            valore = 10;
+                            break;
+
+                        case "ds":
+                            valore = 9;
+                            break;
+
+                        case "b":
+                            valore = 8;
+                            break;
+
+                        case "dc":
+                            valore = 7;
+                            break;
+
+                        case "s":
+                            valore = 6;
+                            break;
+
+                        case "i":
+                            valore = 5;
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                valore = (float?)value;
+            }
+
+
+
+
+
+            return valore;
+        }
     }
+
 }
