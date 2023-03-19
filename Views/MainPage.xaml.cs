d865dc0b8f5d778fad899d2e7c92377f76758e6a
Gabriele <gabboxl0@gmail.com>
2023-03-19 23:26:22 +0100

    Added new navigationview logic from UWP template

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -8,6 +8,18 @@ using Windows.UI.Xaml;
 using ClassevivaPCTO.Utils;
 using ClassevivaPCTO.ViewModel;
 using Windows.ApplicationModel;
+using Windows.System;
+using Windows.UI.Xaml.Input;
+
+using ClassevivaPCTO.Helpers;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using ClassevivaPCTO.Services;
+
+using WinUI = Microsoft.UI.Xaml.Controls;
+using System.Linq;
 
 namespace ClassevivaPCTO
 {
@@ -16,9 +28,36 @@ namespace ClassevivaPCTO
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private readonly KeyboardAccelerator _altLeftKeyboardAccelerator = BuildKeyboardAccelerator(VirtualKey.Left, VirtualKeyModifiers.Menu);
+        private readonly KeyboardAccelerator _backKeyboardAccelerator = BuildKeyboardAccelerator(VirtualKey.GoBack);
+
+        private bool _isBackEnabled;
+        private NavigationViewItem _selected;
+
+        public bool IsBackEnabled
+        {
+            get { return _isBackEnabled; }
+            set { Set(ref _isBackEnabled, value); }
+        }
+
+        public NavigationViewItem Selected
+        {
+            get { return _selected; }
+            set { Set(ref _selected, value); }
+        }
+
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            DataContext = this;
+            Initialize();
+
+        }
+
+        private void Initialize()
+        {
             // Hide default title bar.
             var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
             coreTitleBar.ExtendViewIntoTitleBar = true;
@@ -34,92 +73,132 @@ namespace ClassevivaPCTO
             titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
 
 
-            nvSample.BackRequested += new Windows.Foundation.TypedEventHandler<Microsoft.UI.Xaml.Controls.NavigationView, Microsoft.UI.Xaml.Controls.NavigationViewBackRequestedEventArgs>((Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewBackRequestedEventArgs args) => { NavView_BackRequested(sender, args); });
-            contentFrame.Navigated += On_Navigated;
-
-
 
             LoginResult loginResult = ViewModelHolder.getViewModel().LoginResult;
 
             PersonPictureDashboard.DisplayName = VariousUtils.UppercaseFirst(loginResult.FirstName) + " " + VariousUtils.UppercaseFirst(loginResult.LastName);
 
+
+
+            NavigationService.Frame = shellFrame;
+            NavigationService.NavigationFailed += Frame_NavigationFailed;
+            NavigationService.Navigated += Frame_Navigated;
+            navigationView.BackRequested += OnBackRequested;
+
         }
 
-        private void NavigationView_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
+
+
+        private async void OnLoaded(object sender, RoutedEventArgs e)
         {
-            if (args.IsSettingsSelected)
+            // Keyboard accelerators are added here to avoid showing 'Alt + left' tooltip on the page.
+            // More info on tracking issue https://github.com/Microsoft/microsoft-ui-xaml/issues/8
+            KeyboardAccelerators.Add(_altLeftKeyboardAccelerator);
+            KeyboardAccelerators.Add(_backKeyboardAccelerator);
+            await Task.CompletedTask;
+        }
+
+        private void Frame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw e.Exception;
+        }
+
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
+            IsBackEnabled = NavigationService.CanGoBack;
+            if (e.SourcePageType == typeof(SettingsPage))
             {
-                if (contentFrame.CurrentSourcePageType != typeof(DashboardPage))
+                Selected = navigationView.SettingsItem as WinUI.NavigationViewItem;
+                return;
+            }
+
+            var selectedItem = GetSelectedItem(navigationView.MenuItems, e.SourcePageType);
+            if (selectedItem != null)
+            {
+                Selected = selectedItem;
+            }
+        }
+
+        private NavigationViewItem GetSelectedItem(IEnumerable<object> menuItems, Type pageType)
+        {
+            foreach (var item in menuItems.OfType<WinUI.NavigationViewItem>())
+            {
+                if (IsMenuItemForPageType(item, pageType))
                 {
-                    contentFrame.Navigate(typeof(DashboardPage));
+                    return item;
                 }
+
+                var selectedChild = GetSelectedItem(item.MenuItems, pageType);
+                if (selectedChild != null)
+                {
+                    return selectedChild;
+                }
+            }
+
+            return null;
+        }
+
+        private bool IsMenuItemForPageType(NavigationViewItem menuItem, Type sourcePageType)
+        {
+            var pageType = menuItem.GetValue(NavHelper.NavigateToProperty) as Type;
+            return pageType == sourcePageType;
+        }
+
+        private void OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                NavigationService.Navigate(typeof(SettingsPage), null, args.RecommendedNavigationTransitionInfo);
             }
             else
             {
-                var selectedItem = (Microsoft.UI.Xaml.Controls.NavigationViewItem)args.SelectedItem;
-                if (selectedItem != null)
-                {
-                    string selectedItemTag = ((string)selectedItem.Tag);
+                var selectedItem = args.InvokedItemContainer as NavigationViewItem;
+                var pageType = selectedItem?.GetValue(NavHelper.NavigateToProperty) as Type;
 
-                    string pageName = "ClassevivaPCTO." + selectedItemTag;
-                    Type pageType = Type.GetType(pageName);
-                    contentFrame.Navigate(pageType, null, args.RecommendedNavigationTransitionInfo);
+                if (pageType != null)
+                {
+                    NavigationService.Navigate(pageType, null, args.RecommendedNavigationTransitionInfo);
                 }
             }
-
         }
 
-        private void On_Navigated(object sender, NavigationEventArgs e)
+        private void OnBackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
         {
-            
-            nvSample.IsBackEnabled = contentFrame.CanGoBack;
-            //nvSample.IsBackButtonVisible = contentFrame.CanGoBack ? Microsoft.UI.Xaml.Controls.NavigationViewBackButtonVisible.Visible : Microsoft.UI.Xaml.Controls.NavigationViewBackButtonVisible.Collapsed;
+            NavigationService.GoBack();
+        }
 
-            /* if (contentFrame.SourcePageType == typeof(SettingsPage))
+        private static KeyboardAccelerator BuildKeyboardAccelerator(VirtualKey key, VirtualKeyModifiers? modifiers = null)
+        {
+            var keyboardAccelerator = new KeyboardAccelerator() { Key = key };
+            if (modifiers.HasValue)
             {
-                // SettingsItem is not part of NavView.MenuItems, and doesn't have a Tag.
-                NavView.SelectedItem = (NavigationViewItem)NavView.SettingsItem;
-                NavView.Header = "Settings";
+                keyboardAccelerator.Modifiers = modifiers.Value;
             }
-            else  */
-            if (contentFrame.SourcePageType != null)
+
+            keyboardAccelerator.Invoked += OnKeyboardAcceleratorInvoked;
+            return keyboardAccelerator;
+        }
+
+        private static void OnKeyboardAcceleratorInvoked(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs args)
+        {
+            var result = NavigationService.GoBack();
+            args.Handled = result;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
             {
-                
-                //nvSample.SelectedItem = e.SourcePageType;
-
-                //nvSample.Header = "tony";
+                return;
             }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
         }
 
-
-
-
-
-
-            private void NavView_BackRequested(Microsoft.UI.Xaml.Controls.NavigationView sender,
-                               Microsoft.UI.Xaml.Controls.NavigationViewBackRequestedEventArgs args)
-        {
-            TryGoBack();
-        }
-
-        private bool TryGoBack()
-        {
-            if (!contentFrame.CanGoBack)
-                return false;
-
-            // Don't go back if the nav pane is overlayed.
-            if (nvSample.IsPaneOpen &&
-                (nvSample.DisplayMode == Microsoft.UI.Xaml.Controls.NavigationViewDisplayMode.Compact ||
-                 nvSample.DisplayMode == Microsoft.UI.Xaml.Controls.NavigationViewDisplayMode.Minimal))
-                return false;
-
-            contentFrame.GoBack();
-            return true;
-        }
-
-
-
-
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
 
 
