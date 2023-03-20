1a4b181029b79155cc22fe3616cafa348d1a12d8
Gabriele <gabboxl0@gmail.com>
2023-03-20 22:55:30 +0100

    Added activation services and first run dialog
    from the studio template extension

---
+++ ClassevivaPCTO/Views/FirstRunDialog.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ClassevivaPCTO.Views
+{
+    public sealed partial class FirstRunDialog : ContentDialog
+    {
+        public FirstRunDialog()
+        {
+            // TODO: Update the contents of this dialog with any important information you want to show when the app is used for the first time.
+            RequestedTheme = (Window.Current.Content as FrameworkElement).RequestedTheme;
+            InitializeComponent();
+        }
+    }
+}
