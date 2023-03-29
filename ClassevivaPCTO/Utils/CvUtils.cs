9fafaf5d914220d984e902a73395d74fd93ac8f3
Gabriele <gabboxl0@gmail.com>
2023-03-29 19:07:28 +0200

    Move a class to a file and add some viewmodel properties

---
+++ ClassevivaPCTO/Utils/CvUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ClassevivaPCTO.Utils
+{
+    public class CvUtils
+    {
+        public string GetCode(string userId)
+        {
+            return Regex.Replace(userId, @"[A-Za-z]+", "");
+
+        }
+
+
+    }
+
+}
