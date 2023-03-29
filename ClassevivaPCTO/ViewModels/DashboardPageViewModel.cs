6e52c327e0031c68c9a19093b9936c1785ed23b4
Gabriele <gabboxl0@gmail.com>
2023-03-29 18:33:48 +0200

    Created DashboardPageViewModel

---
+++ ClassevivaPCTO/ViewModels/DashboardPageViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+using ClassevivaPCTO.Helpers;
+using ClassevivaPCTO.Services;
+using ClassevivaPCTO.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Controls;
+using Windows.System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+
+using WinUI = Microsoft.UI.Xaml.Controls;
+
+namespace ClassevivaPCTO.ViewModels
+{
+    public class DashboardPageViewModel : ObservableObject
+    {
+
+
+        private bool _isLoading = false;
+
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            set
+            {
+                SetProperty(ref _isLoading, value);
+
+            }
+        }
+
+        public DashboardPageViewModel()
+        {
+        }
+
+        public void Initialize(Frame frame, WinUI.NavigationView navigationView, IList<KeyboardAccelerator> keyboardAccelerators)
+        {
+
+        }
+
+
+ 
+    }
+}
