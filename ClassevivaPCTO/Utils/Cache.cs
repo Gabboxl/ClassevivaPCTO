b493f8bf26f9a42b9781db761ac7a5710bfe16e3
Gabriele <gabboxl0@gmail.com>
2023-03-30 23:14:06 +0200

    Implemented dependency injection with Refit and Polly
    - for api request resilience
    - added test cache class

---
+++ ClassevivaPCTO/Utils/Cache.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching.Hosting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassevivaPCTO.Utils
+{
+    internal class Cache
+    {
+        private const string KEY = "login_cache";   
+        private readonly IMemoryCache memoryCache;
+
+        public Cache(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+
+        }
+
+
+        public void AddToCache(LoginResult loginResult)
+        {
+            var option = new MemoryCacheEntryOptions
+            {
+                //SlidingExpiration = TimeSpan.FromSeconds(1),
+                AbsoluteExpiration = loginResult.Expire
+            };
+
+            memoryCache.Set<LoginResult>(KEY, loginResult, option);
+        }
+
+        public LoginResult GetCachedLoginResult()
+        {
+            LoginResult loginResult = null;
+            memoryCache.TryGetValue(KEY, out loginResult);
+
+            return loginResult;
+        }
+    }
+}
