6ad3ce42b7140c57b3f84787cb29a00dbc360597
Gabriele <gabboxl0@gmail.com>
2023-03-30 23:35:29 +0200

    Added AgendaViewModel class
    + remove unused imports
    + fixes

---
+++ ClassevivaPCTO/ViewModels/AgendaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ClassevivaPCTO.ViewModels
+{
+    public class AgendaViewModel : ObservableObject
+    {
+
+        private bool _isLoadingAgenda = true;
+        public bool IsLoadingAgenda
+        {
+            get { return _isLoadingAgenda; }
+            set { SetProperty(ref _isLoadingAgenda, value); }
+        }
+
+        public AgendaViewModel()
+        {
+        }
+
+
+
+ 
+    }
+}
