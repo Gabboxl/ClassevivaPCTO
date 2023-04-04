13333137e97b06ce7b3efdf939938f734f15801f
Gabriele <gabboxl0@gmail.com>
2023-04-04 19:34:24 +0200

    Using CommunityToolkit's IsFirstRun property

--- ClassevivaPCTO/Services/FirstRunDisplayService.cs
+++ ClassevivaPCTO/Services/FirstRunDisplayService.cs
@@ -1,4 +1,5 @@
 using ClassevivaPCTO.Views;
+using CommunityToolkit.WinUI.Helpers;
 using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
 
@@ -13,12 +14,12 @@ namespace ClassevivaPCTO.Services
             await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
                 CoreDispatcherPriority.Normal, async () =>
                 {
-                    //if (ApplicationDeployment.CurrentDeployment.isFirstRun && !shown)
-                    //{
+                    if (SystemInformation.Instance.IsFirstRun && !shown)
+                    {
                         shown = true;
                         var dialog = new FirstRunDialog();
                         await dialog.ShowAsync();
-                    //}
+                    }
                 });
         }
     }
