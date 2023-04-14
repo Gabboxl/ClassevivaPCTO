c11b5f015ea2ee332c30cf49e277aae9c5d2776e
Gabriele <gabboxl0@gmail.com>
2023-04-15 01:20:27 +0200

    Added test listview adapter + using custom bindablebase lib
    probably should use INotifyPropertyChanged custom implementation instead?

--- ClassevivaPCTO/Utils/CvAPI.cs
+++ ClassevivaPCTO/Utils/CvAPI.cs
@@ -8,6 +8,20 @@ using System.Threading.Tasks;
 namespace ClassevivaPCTO.Utils
 {
 
+    public class AgendaEventAdapter : BindableBase.BindableBase
+    {
+        private AgendaEvent _event;
+
+        public string Title => _event.notes + " by " + _event.authorName;
+        public string SubjectDesc => _event.subjectDesc;
+
+        public AgendaEventAdapter(AgendaEvent ev)
+        {
+            _event = ev;
+        }
+    }
+
+
     public static class Endpoint
     {
         public static string Official = "https://web.spaggiari.eu/rest/v1";
