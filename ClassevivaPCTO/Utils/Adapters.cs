488a6dd8496b908f7a1ead087bc6d7c54be0efd4
Gabriele <gabboxl0@gmail.com>
2023-04-15 01:23:21 +0200

    Move adapters to Adapters.cs

---
+++ ClassevivaPCTO/Utils/Adapters.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassevivaPCTO.Utils
+{
+    public class AgendaEventAdapter : BindableBase.BindableBase
+    {
+        private AgendaEvent _event;
+
+        public string Title => _event.notes + " by " + _event.authorName;
+        public string SubjectDesc => _event.subjectDesc;
+
+        public AgendaEventAdapter(AgendaEvent ev)
+        {
+            _event = ev;
+        }
+    }
+
+}
