c11a9352e25d5340ccaca4648a08c76e277d2c32
Gabriele <gabboxl0@gmail.com>
2023-04-15 08:58:51 +0200

    Updates to converters

---
+++ ClassevivaPCTO/Converters/DateTimeToHourConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace ClassevivaPCTO.Converters
+{
+
+
+    public class DateTimeToHourConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            DateTime date = (DateTime)value;
+
+            return date.ToString("hh:mm");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return new NotImplementedException();
+        }
+    }
+}
