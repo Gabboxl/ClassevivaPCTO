79810fd8f0b98675af51c8b8464fed99b9ceb1e6
Gabriele <gabboxl0@gmail.com>
2023-04-16 17:43:54 +0200

    Added lessons listview + code

---
+++ ClassevivaPCTO/Adapters/LessonAdapter.cs
@@ -0,0 +1,42 @@
+﻿using ClassevivaPCTO.Utils;
+
+namespace ClassevivaPCTO.Adapters
+{
+    public class LessonAdapter : BindableBase.BindableBase
+    {
+        public Lesson CurrentObject;
+
+        public string Title { 
+            get {
+
+                if(string.IsNullOrEmpty(CurrentObject.subjectDesc))
+                {
+                    return CurrentObject.authorName;
+                } else
+                {
+                    return CurrentObject.subjectDesc + " (" + CurrentObject.authorName + ")";
+                }
+
+            
+            }
+        }
+
+        public string EventType
+        {
+            get
+            {
+
+                return "[" + CurrentObject.evtCode + "]";
+
+
+            }
+        }
+
+
+        public LessonAdapter(Lesson ev)
+        {
+            CurrentObject = ev;
+        }
+    }
+
+}
