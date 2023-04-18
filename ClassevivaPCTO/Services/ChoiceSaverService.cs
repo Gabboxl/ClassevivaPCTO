b027a5aa0bcc5c8ebadd49a48444ab2716ac2d44
Gabriele <gabboxl0@gmail.com>
2023-04-19 00:51:08 +0200

    Added ChoiceSaverService to save login account data

---
+++ ClassevivaPCTO/Services/ChoiceSaverService.cs
@@ -0,0 +1,27 @@
+﻿using ClassevivaPCTO.Helpers;
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace ClassevivaPCTO.Services
+{
+    public static class ChoiceSaverService
+    {
+        private const string SettingsKey = "AppLoginAccountChoice";
+
+        private static async Task<int?> LoadThemeFromSettingsAsync()
+        {
+            int? choice = await ApplicationData.Current.LocalSettings.ReadAsync<int>(SettingsKey);
+
+            return choice;
+        }
+
+        private static async Task SaveThemeInSettingsAsync(int choice)
+        {
+            await ApplicationData.Current.LocalSettings.SaveAsync(SettingsKey, choice.ToString());
+        }
+    }
+}
--- ClassevivaPCTO/Views/LoginPage.xaml.cs
+++ ClassevivaPCTO/Views/LoginPage.xaml.cs
@@ -246,6 +246,8 @@ namespace ClassevivaPCTO.Views
             
 
             var result = await dialog.ShowAsync();
+
+
         }
 
 
