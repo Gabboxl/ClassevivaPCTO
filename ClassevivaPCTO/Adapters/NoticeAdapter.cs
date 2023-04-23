145d56a68022661b1c31e72002eb07dc99d99ff1
Gabriele <gabboxl0@gmail.com>
2023-04-23 14:27:45 +0200

    Implemented Notices listview
    also unified cards loading for dashboard page

---
+++ ClassevivaPCTO/Adapters/NoticeAdapter.cs
@@ -0,0 +1,15 @@
+﻿using ClassevivaPCTO.Utils;
+
+namespace ClassevivaPCTO.Adapters
+{
+    public class NoticeAdapter
+    {
+        public Notice CurrentObject;
+
+
+        public NoticeAdapter(Notice ev)
+        {
+            CurrentObject = ev;
+        }
+    }
+}
