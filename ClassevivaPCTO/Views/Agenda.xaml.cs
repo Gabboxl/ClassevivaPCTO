10c59063c16b575a581d17af3e69281bb13d054f
Gabriele <gabboxl0@gmail.com>
2023-04-23 15:09:14 +0200

    Implemented agenda data loading + layout

--- ClassevivaPCTO/Views/Agenda.xaml.cs
+++ ClassevivaPCTO/Views/Agenda.xaml.cs
@@ -1,7 +1,12 @@
-﻿using ClassevivaPCTO.Utils;
+﻿using ClassevivaPCTO.Adapters;
+using ClassevivaPCTO.Utils;
 using ClassevivaPCTO.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 using Refit;
 using System;
+using System.Linq;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -16,9 +21,16 @@ namespace ClassevivaPCTO.Views
     {
         public AgendaViewModel AgendaViewModel { get; } = new AgendaViewModel();
 
+        private readonly IClassevivaAPI apiWrapper;
+
         public Agenda()
         {
             this.InitializeComponent();
+
+            App app = (App)App.Current;
+            var apiClient = app.Container.GetService<IClassevivaAPI>();
+
+            apiWrapper = PoliciesDispatchProxy<IClassevivaAPI>.CreateProxy(apiClient);
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
@@ -26,6 +38,10 @@ namespace ClassevivaPCTO.Views
             base.OnNavigatedTo(e);
 
             AgendaViewModel.IsLoadingAgenda = true;
+
+
+            //listender for calendaragenda date change
+            CalendarAgenda.DateChanged += CalendarAgenda_DateChanged;
 
             //imposto la data di oggi del picker
             CalendarAgenda.Date = DateTime.Now;
@@ -47,6 +63,80 @@ namespace ClassevivaPCTO.Views
             );
 
             AgendaViewModel.IsLoadingAgenda = false;
+
+
+        }
+
+        private async void CalendarAgenda_DateChanged(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
+        {
+            //if the date is today, then the button to go to today is disabled
+            if (CalendarAgenda.Date.Value.Date == DateTime.Now.Date)
+            {
+                ButtonToday.IsChecked = true;
+                //ButtonToday.IsEnabled = false;
+            }
+            else
+            {
+                ButtonToday.IsChecked = false;
+                //ButtonToday.IsEnabled = true;
+            }
+
+
+
+            LoginResultComplete loginResult = ViewModelHolder.getViewModel().LoginResult;
+            Card cardResult = ViewModelHolder.getViewModel().CardsResult.Cards[0];
+
+            string caldate = VariousUtils.ToApiDateTime(CalendarAgenda.Date.Value.Date);
+            OverviewResult overviewResult = await apiWrapper.GetOverview(
+                cardResult.usrId.ToString(),
+                caldate,
+                caldate,
+                loginResult.token.ToString()
+            );
+
+            //update UI on UI thread
+            await CoreApplication.MainView.Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal,
+                async () =>
+                {
+                    //ListViewAbsencesDate.ItemsSource = overviewResult.Grades;
+                    ListViewVotiDate.ItemsSource = overviewResult.Grades;
+
+                    //order lessons by evtHPos
+                    var orderedlessons = overviewResult.Lessons.OrderBy(x => x.evtHPos).ToList();
+
+                    //remove duplicates based on lessonArg and authorname and increment evtDuration it it is a duplicate
+                    foreach (var lesson in orderedlessons.ToList())
+                    {
+                        var duplicates = orderedlessons
+                            .Where(
+                                x =>
+                                    x.lessonArg == lesson.lessonArg
+                                    && x.authorName == lesson.authorName
+                            )
+                            .ToList();
+                        if (duplicates.Count > 1)
+                        {
+                            lesson.evtDuration += duplicates[1].evtDuration;
+                            orderedlessons.Remove(duplicates[1]);
+                        }
+                    }
+
+                    //orderedlessons = orderedlessons.GroupBy(x => x.lessonArg).Select(x => x.First()).ToList();
+
+                    ListViewLezioniDate.ItemsSource = orderedlessons
+                        .Select(les => new LessonAdapter(les))
+                        .ToList();
+
+                    // Wrap each AgendaEvent object in an instance of AgendaEventAdapter and handle null case
+                    var eventAdapters = overviewResult.AgendaEvents
+                        ?.Select(evt => new AgendaEventAdapter(evt))
+                        .ToList();
+
+                    ListViewAgendaDate.ItemsSource = eventAdapters;
+
+                }
+            );
         }
 
         private void ButtonToday_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
