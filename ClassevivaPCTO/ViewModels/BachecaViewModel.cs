f73f89ee8b8f7da612869f851ff72244aa70ce9a
Gabriele <gabboxl0@gmail.com>
2023-04-24 12:57:43 +0200

    Implemented Bacheca page

---
+++ ClassevivaPCTO/ViewModels/BachecaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ClassevivaPCTO.ViewModels
+{
+    public class BachecaViewModel : ObservableObject
+    {
+        private bool _isLoadingBacheca = true;
+        public bool IsLoadingBacheca
+        {
+            get { return _isLoadingBacheca; }
+            set { SetProperty(ref _isLoadingBacheca, value); }
+        }
+
+        public BachecaViewModel() { }
+    }
+}
--- ClassevivaPCTO/Views/Bacheca.xaml.cs
+++ ClassevivaPCTO/Views/Bacheca.xaml.cs
@@ -1,4 +1,14 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using ClassevivaPCTO.Adapters;
+using ClassevivaPCTO.Utils;
+using ClassevivaPCTO.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 // Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -9,9 +19,65 @@ namespace ClassevivaPCTO.Views
     /// </summary>
     public sealed partial class Bacheca : Page
     {
+        public BachecaViewModel BachecaViewModel { get; } = new BachecaViewModel();
+
+        private readonly IClassevivaAPI apiWrapper;
+
         public Bacheca()
         {
             this.InitializeComponent();
+
+            App app = (App)App.Current;
+            var apiClient = app.Container.GetService<IClassevivaAPI>();
+
+            apiWrapper = PoliciesDispatchProxy<IClassevivaAPI>.CreateProxy(apiClient);
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            BachecaViewModel.IsLoadingBacheca = true;
+
+            await Task.Run(async () =>
+            {
+                await LoadData();
+            });
+        }
+
+        private async Task LoadData()
+        {
+            await CoreApplication.MainView.Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal,
+                async () =>
+                {
+                    BachecaViewModel.IsLoadingBacheca = true;
+                }
+            );
+
+            LoginResultComplete loginResult = ViewModelHolder.getViewModel().LoginResult;
+            Card cardResult = ViewModelHolder.getViewModel().CardsResult.Cards[0];
+
+
+            NoticeboardResult noticeboardResult = await apiWrapper.GetNotices(
+                cardResult.usrId.ToString(),
+                loginResult.token.ToString()
+            );
+
+
+            var noticeAdapters = noticeboardResult.Notices?.Select(evt => new NoticeAdapter(evt)).ToList();
+
+            //update UI on UI thread
+            await CoreApplication.MainView.Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal,
+                async () =>
+                {
+                    NoticesListView.ItemsSource = noticeAdapters;
+
+                    BachecaViewModel.IsLoadingBacheca = false;
+                }
+            );
+
         }
     }
 }
