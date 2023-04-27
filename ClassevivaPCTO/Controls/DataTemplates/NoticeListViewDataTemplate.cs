688594302b2c207fe58d15b2062f497a709b693c
Gabriele <gabboxl0@gmail.com>
2023-04-27 15:18:52 +0200

    Added temp AppBarButton and ResourceDictionary code behind
    this is not best practice, will consider other options

---
+++ ClassevivaPCTO/Controls/DataTemplates/NoticeListViewDataTemplate.cs
@@ -0,0 +1,51 @@
+﻿using ClassevivaPCTO.Adapters;
+using ClassevivaPCTO.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ClassevivaPCTO.Controls.DataTemplates
+{
+    internal partial class NoticeListViewDataTemplate : ResourceDictionary
+    {
+        public NoticeListViewDataTemplate()
+        {
+            InitializeComponent();
+        }
+
+
+        //appbarbutton onclick handler
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            var item = (sender as Button).DataContext as NoticeAdapter;
+            var notice = item.CurrentObject;
+
+
+
+            //create and show a custom dialog
+            ContentDialog dialog = new ContentDialog();
+            dialog.Title = "side";
+            dialog.PrimaryButtonText = "OK";
+            dialog.DefaultButton = ContentDialogButton.Primary;
+            dialog.Content =
+                notice.cntTitle;
+
+            try
+            {
+                var result = await dialog.ShowAsync();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+
+
+        }
+    }
+}
