03a975550b3167ff50f4778b229fb76f4ae5497a
Gabriele <gabboxl0@gmail.com>
2023-04-29 01:03:15 +0200

    Added custom contentdialog to view notices details

---
+++ ClassevivaPCTO/Dialogs/NoticeDialogContent.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ClassevivaPCTO.Utils;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ClassevivaPCTO.Dialogs
+{
+    public sealed partial class NoticeDialogContent : Page
+    {
+        Notice CurrentNotice;
+
+        public NoticeDialogContent(Notice notice)
+        {
+            this.InitializeComponent();
+
+
+            CurrentNotice = notice;
+
+            AttachmentsListView.ItemsSource = notice.attachments;
+        }
+
+        private async void ButtonOpen_Click(object sender, RoutedEventArgs e) { }
+
+        private async void ButtonSave_Click(object sender, RoutedEventArgs e) { }
+    }
+}
