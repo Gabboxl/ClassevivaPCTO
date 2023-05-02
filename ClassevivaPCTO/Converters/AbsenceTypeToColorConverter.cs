b3ef8b13402fb6055a78510e3c62583ff8c13fe8
Gabriele <gabboxl0@gmail.com>
2023-05-02 10:45:53 +0200

    Absence item layout changes

---
+++ ClassevivaPCTO/Converters/AbsenceTypeToColorConverter.cs
@@ -0,0 +1,46 @@
+﻿using ClassevivaPCTO.Utils;
+using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace ClassevivaPCTO.Converters
+{
+    public class AbsenceTypeToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            SolidColorBrush brush = new SolidColorBrush();
+
+            AbsenceEventCode valore = (AbsenceEventCode)value;
+
+
+            switch (valore)
+            {
+                case AbsenceEventCode.ABA0:
+                    brush.Color = Colors.Red;
+                    break;
+
+                case AbsenceEventCode.ABR0:
+                    brush.Color = Colors.Orange;
+                    break;
+
+                case AbsenceEventCode.ABR1:
+                    brush.Color = Colors.Teal;
+                    break;
+
+                case AbsenceEventCode.ABU0:
+                    brush.Color = Colors.LightGoldenrodYellow;
+                    break;
+            }
+
+            return brush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return new NotImplementedException();
+        }
+    }
+
+}
