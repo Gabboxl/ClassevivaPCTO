adff8d6fc0a086b74470d0878816785b8ede24fc
Gabriele <gabboxl0@gmail.com>
2023-05-03 09:36:38 +0200

    Create class to generate unique colors
    used for subject types in this case
    
    - need to create a custom converter in the future, so it can be used for other purposes

--- ClassevivaPCTO/Adapters/LessonAdapter.cs
+++ ClassevivaPCTO/Adapters/LessonAdapter.cs
@@ -1,10 +1,34 @@
 ﻿using ClassevivaPCTO.Utils;
+using Windows.UI.Xaml.Media;
 
 namespace ClassevivaPCTO.Adapters
 {
     public class LessonAdapter
     {
         public Lesson CurrentObject;
+
+        public SolidColorBrush ColorBrush
+        {
+            get
+            {
+                SolidColorBrush brush = new SolidColorBrush();
+                ColorGenerator generator = new ColorGenerator(0.5, 0.8, 0.6);
+
+                System.Drawing.Color colorTest = generator.GetColor((int)CurrentObject.subjectId); // generates a unique color for number 1
+
+
+                byte red = (byte)(colorTest.R * 255);
+                byte green = (byte)(colorTest.G * 255);
+                byte blue = (byte)(colorTest.B * 255);
+                byte alpha = (byte)(colorTest.A * 255);
+
+                Windows.UI.Color uwpColor = Windows.UI.Color.FromArgb(colorTest.A, colorTest.B, colorTest.G, colorTest.R);
+
+                brush.Color = uwpColor;
+
+                return brush;
+            }
+        }
 
         public string Title
         {
---
+++ ClassevivaPCTO/Utils/ColorGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    using System.Drawing;
+
+namespace ClassevivaPCTO.Utils
+{
+
+
+    internal class ColorGenerator
+    {
+        private static double goldenRatio = 0.618033988749895;
+
+        private double hue;
+        private double saturation;
+        private double lightness;
+
+        public ColorGenerator(double hue, double saturation, double lightness)
+        {
+            this.hue = hue;
+            this.saturation = saturation;
+            this.lightness = lightness;
+        }
+
+        public Color GetColor(int number)
+        {
+            double hue = (this.hue + goldenRatio * number) % 1.0;
+            double saturation = this.saturation;
+            double lightness = this.lightness;
+            return FromHsl(hue, saturation, lightness);
+        }
+
+        private static Color FromHsl(double hue, double saturation, double lightness)
+        {
+            if (saturation == 0)
+            {
+                int gray = (int)Math.Round(lightness * 255);
+                return Color.FromArgb(gray, gray, gray);
+            }
+
+            double q = lightness < 0.5 ? lightness * (1 + saturation) : lightness + saturation - lightness * saturation;
+            double p = 2 * lightness - q;
+
+            double r = HueToRgb(p, q, hue + 1.0 / 3.0);
+            double g = HueToRgb(p, q, hue);
+            double b = HueToRgb(p, q, hue - 1.0 / 3.0);
+
+            return Color.FromArgb((int)Math.Round(r * 255), (int)Math.Round(g * 255), (int)Math.Round(b * 255));
+        }
+
+        private static double HueToRgb(double p, double q, double t)
+        {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1.0 / 6.0) return p + (q - p) * 6 * t;
+            if (t < 1.0 / 2.0) return q;
+            if (t < 2.0 / 3.0) return p + (q - p) * (2.0 / 3.0 - t) * 6;
+            return p;
+        }
+    }
+
+}
