802d3301c90492fea917ddb6052674af9944b23a
Gabriele <gabboxl0@gmail.com>
2023-05-03 14:53:51 +0200

    Remove comments

--- ClassevivaPCTO/Views/Bacheca.xaml.cs
+++ ClassevivaPCTO/Views/Bacheca.xaml.cs
@@ -10,13 +10,8 @@ using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-// Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace ClassevivaPCTO.Views
 {
-    /// <summary>
-    /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
-    /// </summary>
     public sealed partial class Bacheca : Page
     {
         public BachecaViewModel BachecaViewModel { get; } = new BachecaViewModel();
