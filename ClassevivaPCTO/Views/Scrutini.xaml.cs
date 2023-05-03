c26c4185eb0b2ab1f93d8ee882b17e2554cabd94
Gabriele <gabboxl0@gmail.com>
2023-05-04 00:24:11 +0200

    Added blank Scrutini page

---
+++ ClassevivaPCTO/Views/Scrutini.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ClassevivaPCTO.Views
+{
+    public sealed partial class Scrutini : Page
+    {
+        public Scrutini()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
