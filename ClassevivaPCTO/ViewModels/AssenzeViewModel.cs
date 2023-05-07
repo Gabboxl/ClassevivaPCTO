175a63346cb4eae99864891b046b68da39d5f1ff
Gabriele <gabboxl0@gmail.com>
2023-05-07 19:45:31 +0200

    Added Assenze page base code structure

---
+++ ClassevivaPCTO/ViewModels/AssenzeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ClassevivaPCTO.ViewModels
+{
+    public class AssenzeViewModel : ObservableObject
+    {
+        private bool _isLoadingAssenze = true;
+        public bool IsLoadingAssenze
+        {
+            get { return _isLoadingAssenze; }
+            set { SetProperty(ref _isLoadingAssenze, value); }
+        }
+
+        public AssenzeViewModel() { }
+    }
+}
--- ClassevivaPCTO/Views/Assenze.xaml.cs
+++ ClassevivaPCTO/Views/Assenze.xaml.cs
@@ -1,17 +1,48 @@
-﻿using Windows.UI.Xaml.Controls;
-
-// Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=234238
+﻿using ClassevivaPCTO.Utils;
+using ClassevivaPCTO.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 namespace ClassevivaPCTO.Views
 {
-    /// <summary>
-    /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
-    /// </summary>
     public sealed partial class Assenze : Page
     {
+        public AssenzeViewModel AssenzeViewModel { get; } = new AssenzeViewModel();
+
+        private readonly IClassevivaAPI apiWrapper;
+
         public Assenze()
         {
             this.InitializeComponent();
+
+            App app = (App)App.Current;
+            var apiClient = app.Container.GetService<IClassevivaAPI>();
+
+            apiWrapper = PoliciesDispatchProxy<IClassevivaAPI>.CreateProxy(apiClient);
+        }
+
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            AssenzeViewModel.IsLoadingAssenze = true;
+
+            /*
+            await Task.Run(async () =>
+            {
+                await LoadData();
+            });*/
+        }
+
+        private async void AggiornaCommand_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await Task.Run(async () =>
+            {
+                //await LoadData();
+            });
         }
     }
 }
