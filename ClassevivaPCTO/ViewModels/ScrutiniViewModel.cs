a25046bd388ede48aa7a698e7fb6f02f2291d7ee
Gabriele <gabboxl0@gmail.com>
2023-05-07 19:42:35 +0200

    Added Scrutini page's base code structure

---
+++ ClassevivaPCTO/ViewModels/ScrutiniViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ClassevivaPCTO.ViewModels
+{
+    public class ScrutiniViewModel : ObservableObject
+    {
+        private bool _isLoadingScrutini = true;
+        public bool IsLoadingScrutini
+        {
+            get { return _isLoadingScrutini; }
+            set { SetProperty(ref _isLoadingScrutini, value); }
+        }
+
+        public ScrutiniViewModel() { }
+    }
+}
--- ClassevivaPCTO/Views/Scrutini.xaml.cs
+++ ClassevivaPCTO/Views/Scrutini.xaml.cs
@@ -1,12 +1,47 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using ClassevivaPCTO.Utils;
+using ClassevivaPCTO.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 namespace ClassevivaPCTO.Views
 {
     public sealed partial class Scrutini : Page
     {
+        public ScrutiniViewModel ScrutiniViewModel { get; } = new ScrutiniViewModel();
+
+        private readonly IClassevivaAPI apiWrapper;
+
         public Scrutini()
         {
             this.InitializeComponent();
+
+            App app = (App)App.Current;
+            var apiClient = app.Container.GetService<IClassevivaAPI>();
+
+            apiWrapper = PoliciesDispatchProxy<IClassevivaAPI>.CreateProxy(apiClient);
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            ScrutiniViewModel.IsLoadingScrutini = true;
+
+            /*
+            await Task.Run(async () =>
+            {
+                await LoadData();
+            });*/
+        }
+
+        private async void AggiornaCommand_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await Task.Run(async () =>
+            {
+                //await LoadData();
+            });
         }
     }
 }
