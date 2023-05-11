e80d40d620f16f1f497ceb27f94e4d10596612b0
Gabriele <gabboxl0@gmail.com>
2023-05-11 13:53:51 +0200

    Added Note page

---
+++ ClassevivaPCTO/ViewModels/NoteViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ClassevivaPCTO.ViewModels
+{
+    public class NoteViewModel : ObservableObject
+    {
+        private bool _isLoadingNote = true;
+        public bool IsLoadingNote
+        {
+            get { return _isLoadingNote; }
+            set { SetProperty(ref _isLoadingNote, value); }
+        }
+
+        public NoteViewModel() { }
+    }
+}
---
+++ ClassevivaPCTO/Views/Note.xaml.cs
@@ -0,0 +1,47 @@
+﻿using ClassevivaPCTO.Utils;
+using ClassevivaPCTO.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace ClassevivaPCTO.Views
+{
+    public sealed partial class Note : Page
+    {
+        public NoteViewModel NoteViewModel { get; } = new NoteViewModel();
+
+        private readonly IClassevivaAPI apiWrapper;
+
+        public Note()
+        {
+            this.InitializeComponent();
+
+            App app = (App)App.Current;
+            var apiClient = app.Container.GetService<IClassevivaAPI>();
+
+            apiWrapper = PoliciesDispatchProxy<IClassevivaAPI>.CreateProxy(apiClient);
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            NoteViewModel.IsLoadingNote = true;
+
+            /*
+            await Task.Run(async () =>
+            {
+                await LoadData();
+            });*/
+        }
+
+        private async void AggiornaCommand_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await Task.Run(async () =>
+            {
+                //await LoadData();
+            });
+        }
+    }
+}
