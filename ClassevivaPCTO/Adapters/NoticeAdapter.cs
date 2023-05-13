34c9b10f39d12a0a7894eb6b59d27a26a5415845
Gabriele <gabboxl0@gmail.com>
2023-05-13 18:24:31 +0200

    Implemented notices status text & color

--- ClassevivaPCTO/Adapters/NoticeAdapter.cs
+++ ClassevivaPCTO/Adapters/NoticeAdapter.cs
@@ -8,6 +8,45 @@ namespace ClassevivaPCTO.Adapters
     {
         public readonly Notice CurrentObject;
 
+        public SolidColorBrush StatusTextColor
+        {
+            get
+            {
+                SolidColorBrush brush = new SolidColorBrush();
+
+                if (CurrentObject.cntStatus == "deleted")
+                {
+                    brush.Color = Colors.Crimson;
+                }
+                else if (!CurrentObject.cntValidInRange)
+                {
+                    brush.Color = Colors.DarkOrange;
+                }
+                else
+                {
+                    brush.Color = Colors.Green;
+                }
+
+                return brush;
+            }
+        }
+
+        public string StatusText
+        {
+            get
+            {
+                if (CurrentObject.cntStatus == "deleted")
+                {
+                    return "Comunicazione eliminata";
+                } else if (!CurrentObject.cntValidInRange)
+                {
+                    return "Comunicazione scaduta";
+                }
+
+                return "";
+            }
+        }
+
         public string ReadIcon
         {
             get
