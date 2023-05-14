4e88d490e1bee2de8570d6b7d2e8a02b8fc46612
Gabriele <gabboxl0@gmail.com>
2023-05-14 23:16:34 +0200

    Remove some comments

--- ClassevivaPCTO/Views/DettaglioVoti.xaml.cs
+++ ClassevivaPCTO/Views/DettaglioVoti.xaml.cs
@@ -7,13 +7,8 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-// Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace ClassevivaPCTO.Views
 {
-    /// <summary>
-    /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
-    /// </summary>
     public sealed partial class DettaglioVoti : Page
     {
         Grades2Result grades2Result;
@@ -31,8 +26,6 @@ namespace ClassevivaPCTO.Views
             Card cardResult = ViewModelHolder.getViewModel().SingleCardResult;
 
             var api = RestService.For<IClassevivaAPI>(Endpoint.CurrentEndpoint);
-
-            //string fixedId = new CvUtils().GetCode(loginResult.Ident);
 
             grades2Result = await api.GetGrades(
                 cardResult.usrId.ToString(),
