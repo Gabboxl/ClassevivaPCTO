cf60b4baaaa8fde667fadeac8950115415ab51b4
Gabriele <gabboxl0@gmail.com>
2023-05-14 14:33:42 +0200

    Fix possible null exception

--- ClassevivaPCTO/Dialogs/FirstRunDialog.xaml.cs
+++ ClassevivaPCTO/Dialogs/FirstRunDialog.xaml.cs
@@ -8,7 +8,7 @@ namespace ClassevivaPCTO.Dialogs
         public FirstRunDialog()
         {
             // TODO: Update the contents of this dialog with any important information you want to show when the app is used for the first time.
-            RequestedTheme = (Window.Current.Content as FrameworkElement).RequestedTheme;
+            RequestedTheme = ((FrameworkElement) Window.Current.Content).RequestedTheme;
             InitializeComponent();
         }
     }
