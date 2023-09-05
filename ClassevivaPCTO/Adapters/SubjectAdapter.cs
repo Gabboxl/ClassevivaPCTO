79f694adebec7fac81c2688f0fc197f5aad71e97
Gabriele <gabboxl0@gmail.com>
2023-09-05 18:52:57 +0200

    First implementation of the Valutazioni page's data load

---
+++ ClassevivaPCTO/Adapters/SubjectAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using ClassevivaPCTO.Utils;
+using Windows.UI.Xaml.Media;
+
+namespace ClassevivaPCTO.Adapters
+{
+    public class SubjectAdapter
+    {
+        public readonly List<Grade> SubjectGrades;
+        public readonly Subject Subject;
+
+
+        public SubjectAdapter(Subject subject, List<Grade> subjectSubjectGrades)
+        {
+            SubjectGrades = subjectSubjectGrades;
+            Subject = subject;
+        }
+    }
+}
--- ClassevivaPCTO/Views/Valutazioni.xaml.cs
+++ ClassevivaPCTO/Views/Valutazioni.xaml.cs
@@ -2,21 +2,19 @@
 using ClassevivaPCTO.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using ClassevivaPCTO.Adapters;
 
 
 namespace ClassevivaPCTO.Views
 {
     public sealed partial class Valutazioni : Page
     {
-        private Grades2Result _grades2Result;
-
-        private readonly IClassevivaAPI apiWrapper;
+        private readonly IClassevivaAPI _apiWrapper;
 
         public Valutazioni()
         {
@@ -25,7 +23,7 @@ namespace ClassevivaPCTO.Views
             App app = (App) App.Current;
             var apiClient = app.Container.GetService<IClassevivaAPI>();
 
-            apiWrapper = PoliciesDispatchProxy<IClassevivaAPI>.CreateProxy(apiClient);
+            _apiWrapper = PoliciesDispatchProxy<IClassevivaAPI>.CreateProxy(apiClient!);
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
@@ -37,30 +35,78 @@ namespace ClassevivaPCTO.Views
 
             MainTitleTextBox.Text += VariousUtils.ToTitleCase(cardResult.firstName);
 
-
             await Task.Run(async () =>
             {
-                _grades2Result = await apiWrapper.GetGrades(
+                Grades2Result grades2Result = await _apiWrapper.GetGrades(
                     cardResult.usrId.ToString()
                 );
 
-                var resultPeriods = await apiWrapper.GetPeriods(
+                PeriodsResult resultPeriods = await _apiWrapper.GetPeriods(
                     cardResult.usrId.ToString()
                 );
+
+                SubjectsResult resultSubjects = await _apiWrapper.GetSubjects(
+                    cardResult.usrId.ToString()
+                );
+
+                var grades = grades2Result.Grades;
+                var periods = resultPeriods.Periods;
+                var subjects = resultSubjects.Subjects;
+
+                /*
+                var groupedData = from g in grades
+                    join p in periods on g.periodPos equals p.periodPos
+                    join s in subjects on g.subjectId equals s.id
+                    group g by new {p.periodDesc, s.description}
+                    into g
+                    select new
+                    {
+                        Period = g.Key.periodDesc,
+                        Subject = g.Key.description,
+                        Grades = g.ToList()
+                    };
+                    */
+
+                // Select unique Periods from Grade list
+                var periodList = grades
+                    .GroupBy(g => g.periodPos)
+                    .Select(g => new
+                    {
+                        Period = periods.Single(p => p.periodPos == g.Key),
+                        Subjects = g.GroupBy(s => s.subjectId)
+                            .Select(sub => new
+                            {
+                                Subject = subjects.Single(s => s.id == sub.Key),
+                                Grades = sub.Select(gr => gr).ToList()
+                            }).ToList()
+                    }).ToList();
 
 
                 //run on UI thread
                 await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
                 {
+                    //add to ComboPeriodi every period of resultPeriods
+                    foreach (var period in periodList)
+                    {
+                        SegmentedVoti.Items.Add(VariousUtils.UppercaseFirst(period.Period.periodDesc));
+
+
+                    }
+
+                    var periodGrades = periodList[0].Subjects;
+
+                    //create a list of SubjectAdapter for every subject in periodGrades
+                    var subjectAdapters = periodGrades.Select(subject =>
+                        new SubjectAdapter(subject.Subject, subject.Grades)
+                    ).ToList();
+
+                    MainListView.ItemsSource = subjectAdapters;
+
+
 
                     ProgressRingVoti.Visibility = Visibility.Collapsed;
                 });
             });
         }
-
-
-        
-
-
     }
 }
