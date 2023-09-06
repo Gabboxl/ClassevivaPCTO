69d6f46684a758cff246dffc292b794abbe58828
Gabriele <gabboxl0@gmail.com>
2023-09-06 17:31:12 +0200

    Implement teachers name join + test progressing

--- ClassevivaPCTO/Adapters/SubjectAdapter.cs
+++ ClassevivaPCTO/Adapters/SubjectAdapter.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using ClassevivaPCTO.Utils;
 using Windows.UI.Xaml.Media;
 
@@ -8,6 +10,14 @@ namespace ClassevivaPCTO.Adapters
     {
         public readonly List<Grade> SubjectGrades;
         public readonly Subject Subject;
+
+        public string Teachers
+        {
+            get
+            {
+                return String.Join(", ", Subject.teachers.Select(t => t.teacherName));
+            }
+        }
 
 
         public SubjectAdapter(Subject subject, List<Grade> subjectSubjectGrades)
