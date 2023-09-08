b4c531f0a3d2d7d274e930085eb78e603ec7138d
Gabriele <gabboxl0@gmail.com>
2023-09-08 11:40:27 +0200

    Implement average variation icons + colors (Closes #75)

--- ClassevivaPCTO/Adapters/SubjectAdapter.cs
+++ ClassevivaPCTO/Adapters/SubjectAdapter.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Windows.UI.Xaml.Controls;
 using ClassevivaPCTO.Utils;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using ClassevivaPCTO.Helpers.Palettes;
+using ClassevivaPCTO.Services;
 
 namespace ClassevivaPCTO.Adapters
 {
@@ -13,34 +17,61 @@ namespace ClassevivaPCTO.Adapters
 
         public string Teachers
         {
-            get
-            {
-                return String.Join(", ", Subject.teachers.Select(t => t.teacherName));
-            }
+            get { return String.Join(", ", Subject.teachers.Select(t => t.teacherName)); }
         }
 
         public float Average
         {
-            get
-            {
-                return VariousUtils.CalcolaMedia(SubjectGrades);
-            }
+            get { return VariousUtils.CalcolaMedia(SubjectGrades); }
         }
 
         public string AverageString
         {
-            get
-            {
-                
-                return Average.ToString("0.0");
-            }
+            get { return Average.ToString("0.0"); }
         }
 
         public float Progress
         {
+            get { return (Average * 10); }
+        }
+
+        public FontIcon AverageVariationIcon
+        {
             get
             {
-                return (Average * 10);
+                //get list of grades without the grade that has the most recent date
+                List<Grade> gradesWithoutLastGrade =
+                    SubjectGrades.Where(g => g.evtDate != SubjectGrades.Max(g => g.evtDate)).ToList();
+
+                float averageWithoutLastGrade = VariousUtils.CalcolaMedia(gradesWithoutLastGrade);
+
+                IPalette currentPalette = PaletteSelectorService.PaletteClass;
+
+                SolidColorBrush brush = new SolidColorBrush();
+
+                string iconString;
+
+                if (averageWithoutLastGrade > Average)
+                {
+                    iconString = "\uF0AE";
+                    brush.Color = currentPalette.ColorRed;
+                }
+                else if (averageWithoutLastGrade < Average)
+                {
+                    iconString = "\uF0AD";
+                    brush.Color = currentPalette.ColorGreen;
+                }
+                else
+                {
+                    iconString = "\uE94E";
+                    brush.Color = currentPalette.ColorOrange;
+                }
+
+                FontIcon icon = new FontIcon();
+                icon.Glyph = iconString;
+                icon.Foreground = brush;
+
+                return icon;
             }
         }
 
