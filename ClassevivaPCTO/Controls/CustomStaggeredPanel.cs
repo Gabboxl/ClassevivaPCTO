dae33c4ec9f2d618680513c5d8ecac4a0b76b5c6
Gabriele <gabboxl0@gmail.com>
2023-11-11 12:58:39 +0100

    Refactor DashboardPage layout (Closes #128)
    + use CommunityToolkit's StaggeredLayout (which is more flexible with widgets' heights)
    + remove UniformGridEx usage + code

---
+++ ClassevivaPCTO/Controls/CustomStaggeredPanel.cs
@@ -0,0 +1,20 @@
+﻿using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using CommunityToolkit.WinUI.Controls;
+
+namespace ClassevivaPCTO.Controls
+{
+    public class CustomStaggeredPanel : StaggeredPanel
+    {
+        private const double MaxColumnWidth = 500;
+        //protected override Size MeasureOverride(Size availableSize)
+        //{
+        //    // Calculate the number of columns based on the max column width
+        //    int columns = (int)(availableSize.Width / MaxColumnWidth);
+        //    // Set the desired column width
+        //    this.DesiredColumnWidth = availableSize.Width / columns;
+        //    return base.MeasureOverride(availableSize);
+        //}
+    }
+}
