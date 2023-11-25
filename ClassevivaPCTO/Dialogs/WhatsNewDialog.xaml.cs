0cf6aea231f93198988f66594147567df1528f65
alessandrocaseti <alessandrocaseti@gmail.com>
2023-11-25 18:58:11 +0100

    Moved changelog to Untranlatable.resw file

--- ClassevivaPCTO/Dialogs/WhatsNewDialog.xaml.cs
+++ ClassevivaPCTO/Dialogs/WhatsNewDialog.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using ClassevivaPCTO.Helpers;
 
 namespace ClassevivaPCTO.Dialogs
 {
@@ -10,6 +11,14 @@ namespace ClassevivaPCTO.Dialogs
             // TODO: Update the contents of this dialog every time you release a new version of the app
             RequestedTheme = ((FrameworkElement) Window.Current.Content).RequestedTheme;
             InitializeComponent();
+
+            Highlights.Text = "ChangelogHighlighsSection".GetLocalizedStr(true);
+            General.Text = "ChangelogGeneralSection".GetLocalizedStr(true);
+            Dashboard.Text = "ChangelogDashboardSection".GetLocalizedStr(true);
+            Agenda.Text = "ChangelogAgendaSection".GetLocalizedStr(true);
+            Grades.Text = "ChangelogGradesSection".GetLocalizedStr(true);
+            Absences.Text = "ChangelogAbsencesSection".GetLocalizedStr(true);
+            NoticeBoard.Text = "ChangelogNoticeBoardSection".GetLocalizedStr(true);
         }
     }
 }
