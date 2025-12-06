bbc68d0264773c587ed9a15909182cb36cb84ee3
Gabriele <gabboxl0@gmail.com>
2025-12-06 12:24:59 +0100

    Update copyright

--- ClassevivaPCTO/Properties/AssemblyInfo.cs
+++ ClassevivaPCTO/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ClassevivaPCTO")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
